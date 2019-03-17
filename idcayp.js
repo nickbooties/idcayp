6b6af72c9482f6253c59b6b943f325911238b132
Nick <nickbooties@gmail.com>
2019-03-17 18:29:37 +0100

    First commit. It basically works.
    Using the mutation observer, we find elements which contain "privacy" or "cookie" in their HTML.
    Then we revert the change using the observers oldValue. We also tag the element, so that we don't
    trigger on our own changes.

---
+++ idcayp.js
@@ -0,0 +1,36 @@
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+//delete anything that changes with cookie/privacy in the html
+var observer = new MutationObserver(function(mutations, observer) {
+    mutations.forEach(function(mutation){
+
+      t = mutation.target;
+
+      if(t.className.search(/idcayp/) == -1) {
+
+        if(t.innerHTML.toString().search(/privacy|cookie/ig) >= 0) {
+
+          console.log(mutation);
+          if(mutation.attributeName == "class") {
+            //reset element
+            t.className = mutation.oldValue;
+
+          } else if (mutation.attributeName == "style") {
+            t.style = mutation.oldValue;
+          }
+
+          //tag anything we may have touched.
+          t.className = t.className+" idcayp";
+
+        }
+      }
+    });
+});
+
+observer.observe(document, {
+  subtree: true,
+  childlist:true,
+  attributes: true,
+  attributeOldValue: true
+});
+console.log('loaded');
